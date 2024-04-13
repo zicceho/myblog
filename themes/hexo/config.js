8bdfab915db5ab0913c3f19769cb7cf97dbbb256
zicceho <165814501+zicceho@users.noreply.github.com>
2024-04-13 21:53:22 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HEXO_HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
 
-  HEXO_HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
+  HEXO_HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
   // 已知未修复bug, 在移动端开启true后会加载不出图片； 暂时建议设置为false。
   HEXO_HOME_NAV_BACKGROUND_IMG_FIXED: false, // 首页背景图滚动时是否固定，true 则滚动时图片不懂动； false则随鼠标滚动 ;
   // 是否显示开始阅读按钮
@@ -25,8 +25,8 @@ const CONFIG = {
   HEXO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
 
   HEXO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
-  HEXO_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
-  HEXO_ARTICLE_RECOMMEND: true, // 文章关联推荐
+  HEXO_ARTICLE_COPYRIGHT: false, // 显示文章版权声明
+  HEXO_ARTICLE_RECOMMEND: false, // 文章关联推荐
 
   HEXO_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
   HEXO_WIDGET_ANALYTICS: false, // 显示统计卡

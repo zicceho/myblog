d620cfb03308fba519f33fe67bc8530614f30ab2
zicceho <165814501+zicceho@users.noreply.github.com>
2024-04-13 23:22:51 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -53,48 +53,25 @@ const BLOG = {
   // 默认下会将你上传到 notion的主页封面图和头像也给替换，建议将主页封面图和头像放在其他图床，在 notion 里配置 link 即可。
 
   // START ************网站字体*****************
+// ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
+// 后面空格隔开的 font-light 是字体粗细，留空是默认粗细；参考 https://www.tailwindcss.cn/docs/font-weight
+FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-serif', // 字体是否有衬线：font-sans，font-serif
 
-FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans', // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
-// 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
 FONT_URL: [
-  'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
-  'https://fonts.googleapis.com/css?family=Bitter&display=swap',
-  'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
-  'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
+  'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css', // 字体CSS
+  // 在这里添加其他字体的 URL，如果需要的话
 ],
+
 // 无衬线字体 例如'"LXGW WenKai"'
 FONT_SANS: [
-  '"LXGW WenKai"',
-  '"PingFang SC"',
-  '-apple-system',
-  'BlinkMacSystemFont',
-  '"Hiragino Sans GB"',
-  '"Microsoft YaHei"',
-  '"Segoe UI Emoji"',
-  '"Segoe UI Symbol"',
-  '"Segoe UI"',
-  '"Noto Sans SC"',
-  'HarmonyOS_Regular',
-  '"Helvetica Neue"',
-  'Helvetica',
-  '"Source Han Sans SC"',
-  'Arial',
-  'sans-serif',
-  '"Apple Color Emoji"'
+  '"LXGW WenKai"'
 ],
+
 // 衬线字体 例如'"LXGW WenKai"'
 FONT_SERIF: [
-  '"LXGW WenKai"',
-  'Bitter',
-  '"Noto Serif SC"',
-  'SimSun',
-  '"Times New Roman"',
-  'Times',
-  'serif',
-  '"Segoe UI Emoji"',
-  '"Segoe UI Symbol"',
-  '"Apple Color Emoji"'
+  '"LXGW WenKai"'
 ],
+
 FONT_AWESOME: process.env.NEXT_PUBLIC_FONT_AWESOME_PATH || 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css', // font-awesome 字体图标地址; 可选 /css/all.min.css ， https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css
 
 // END ************网站字体*****************

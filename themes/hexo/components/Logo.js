26b77f48e76b4117dc3ec7e8da0fd438eb487c57
zicceho <165814501+zicceho@users.noreply.github.com>
2024-06-23 17:14:31 +0800

    Update Logo.js

--- themes/hexo/components/Logo.js
+++ themes/hexo/components/Logo.js
@@ -1,20 +1,13 @@
 import { siteConfig } from '@/lib/config'
 import Link from 'next/link'
-/**
- * Logo
- * 实际值支持文字
- * @param {*} props
- * @returns
- */
+
 const Logo = props => {
-  const { siteInfo } = props
   return (
     <Link href='/' passHref legacyBehavior>
       <div className='flex flex-col justify-center items-center cursor-pointer space-y-3'>
-        <div className='font-medium text-lg p-1.5 rounded dark:border-white dark:text-white menu-link transform duration-200'>
-          {' '}
-          {siteInfo?.title || siteConfig('TITLE')}
-        </div>
+        {/* 二选一，顶部左上角显示是文字还是图片logo */}
+        {/* <div className='font-medium text-lg p-1.5 rounded dark:border-white dark:text-white menu-link transform duration-200'>{siteConfig('TITLE')}</div> */}
+        <img src='/avatar.png' className='w-auto h-10'/>
       </div>
     </Link>
   )

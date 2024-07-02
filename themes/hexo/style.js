890a0da20cee8a70241595d886aeeb857891bbbf
zicceho <165814501+zicceho@users.noreply.github.com>
2024-07-02 19:52:43 +0800

    Update style.js

--- themes/hexo/style.js
+++ themes/hexo/style.js
@@ -1,77 +1,144 @@
 /* eslint-disable react/no-unknown-property */
 /**
- * 这里的css样式只对当前主题生效
- * 主题客制化css
+ * 这里的 css 样式只对当前主题生效
+ * 主题客制化 css
  * @returns
  */
 const Style = () => {
-  return (<style jsx global>{`
-    // 底色
-    body{
-        background-color: #f5f5f5
-    }
-    .dark body{
-        background-color: black;
-    }
-  
-    /*  菜单下划线动画 */
-    #theme-hexo .menu-link {
-        text-decoration: none;
-        background-image: linear-gradient(#F2AF3C, #F2AF3C);
-        background-repeat: no-repeat;
-        background-position: bottom center;
-        background-size: 0 2px;
-        transition: background-size 100ms ease-in-out;
-    }
-    
-    #theme-hexo .menu-link:hover {
-        background-size: 0% 2px;
-        color: #F2AF3C;
-    }
+  return (
+    <style jsx global>
+      {`
+        // 底色
+        body{
+            background-color: #f5f5f5
+        }
+       .dark body{
+            background-color: black;
+        }
+      
+        /*  菜单下划线动画 */
+        #theme-hexo.menu-link {
+            text-decoration: none;
+            background-image: linear-gradient(#F2AF3C, #F2AF3C);
+            background-repeat: no-repeat;
+            background-position: bottom center;
+            background-size: 0 2px;
+            transition: background-size 100ms ease-in-out;
+        }
+        
+        #theme-hexo.menu-link:hover {
+            background-size: 0% 2px;
+            color: #F2AF3C;
+        }
 
-    /* 设置了从上到下的渐变黑色 */
-    #theme-hexo .header-cover::before {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background:  linear-gradient(to bottom, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.2) 10%, rgba(0,0,0,0) 25%, rgba(0,0,0,0.2) 75%, rgba(0,0,0,0.5) 100%);
-    }
+        /* 设置了从上到下的渐变黑色 */
+        #theme-hexo.header-cover::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background:  linear-gradient(to bottom, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.2) 10%, rgba(0,0,0,0) 25%, rgba(0,0,0,0.2) 75%, rgba(0,0,0,0.5) 100%);
+        }
 
-    /* Custem */
-    .tk-footer{
-        opacity: 0;
-    }
+        /* Custem */
+       .tk-footer{
+            opacity: 0;
+        }
 
-    // 选中字体颜色
-    ::selection {
-        background: #F2AF3C;
-    color: #FFFFFF; /* 白色 */
-}
+        // 选中字体颜色
+        ::selection {
+            background: #F2AF3C;
+            color: #FFFFFF; /* 白色 */
+        }
 
-    // 自定义滚动条
-    ::-webkit-scrollbar {
-        width: 5px;
-        height: 5px;
-    }
+        // 自定义滚动条
+        ::-webkit-scrollbar {
+            width: 5px;
+            height: 5px;
+        }
 
-    ::-webkit-scrollbar-track {
-        background: transparent;
-    }
+        ::-webkit-scrollbar-track {
+            background: transparent;
+        }
 
-    ::-webkit-scrollbar-thumb {
-        background-color: #e1e3e6;
-    }
+        ::-webkit-scrollbar-thumb {
+            background-color: #e1e3e6;
+        }
 
-    * {
-        scrollbar-width:thin;
-        scrollbar-color: #e1e3e6 transparent
-    }
-    
+        * {
+            scrollbar-width:thin;
+            scrollbar-color: #e1e3e6 transparent
+        }
 
-  `}</style>)
-}
+        /* 导航栏签颜色 */
+        #sticky-nav.bg-indigo-700 {
+            background-color: #F2AF3C;
+        }
 
-export { Style }
+        /* 标签颜色 */
+        a.cursor-pointer.bg-indigo-700 {
+            background-color: #F2AF3C;
+        }
+
+        /* 移动端侧边栏 */
+        #side-bar>.bg-indigo-700 {
+            background-color: #F2AF3C;
+        }
+
+        /* 移动端侧边栏菜单 */
+        nav div:hover,li:hover {
+            background-color: #F2AF3C!important;
+        }
+
+        /* 悬浮菜单 */
+       right-2.bg-indigo-700 {
+            background-color: #F2AF3C;
+        }
+
+        /* 目录进度条 */
+       h-4.bg-indigo-400 {
+            background-color: #F2AF3C;
+        }
+
+        /* 目录文字高亮 */
+        nav.text-green-500{
+            color: #F2AF3C;
+        }
+
+        /* 网站页脚 */ 
+        footer.bg-indigo-700{
+            background-color: #F2AF3C;
+        }
+
+        /* 翻页按钮 */
+        button.bg-indigo-700{
+            background-color: #F2AF3C;
+        }
+
+        /* 文章页隐藏更新时间 */
+        #article-wrapper > div.wow.fadeInUp.px-10 > section > div.flex.flex-wrap.gap-3.mt-5.text-sm > span.whitespace-nowrap {
+          display: none;
+        }
+
+        /* 文章页隐藏阅读次数 */
+        #article-wrapper.busuanzi_container_page_pv {
+          display: none ！important;
+        }
+
+        /* 文章页隐藏字数 */
+        #wordCountWrapper > span:nth-child(1){
+         display: none;
+        }
+
+        /* 文章页隐藏阅读时长 */
+        #wordCountWrapper > span:nth-child(2){
+         display: none;
+        }  
+      `}
+    </style>
+  );
+};
+
+export { Style };

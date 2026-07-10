72690b61e9bcb89dccc8906f53f69675986dccdd
tangly1024 <mail@tangly1024.com>
2026-07-10 17:25:03 +0800

    fix hexo aos refresh after route change

--- components/AOSAnimation.js
+++ components/AOSAnimation.js
@@ -1,6 +1,17 @@
 import { loadExternalResource } from '@/lib/utils'
+import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 // import AOS from 'aos'
+
+const refreshAOS = () => {
+  if (!window.AOS) return
+
+  if (window.AOS.refreshHard) {
+    window.AOS.refreshHard()
+  } else if (window.AOS.refresh) {
+    window.AOS.refresh()
+  }
+}
 
 /**
  * 加载滚动动画
@@ -8,6 +19,7 @@ import { useEffect } from 'react'
  * https://michalsnik.github.io/aos/
  */
 export default function AOSAnimation() {
+  const router = useRouter()
   const initAOS = () => {
     if (
       window.matchMedia &&
@@ -38,6 +50,17 @@ export default function AOSAnimation() {
     const id = window.setTimeout(initAOS, 2000)
     return () => window.clearTimeout(id)
   }, [])
+
+  useEffect(() => {
+    const handleRouteChangeComplete = () => {
+      window.requestAnimationFrame(refreshAOS)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChangeComplete)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChangeComplete)
+    }
+  }, [router.events])
 
   return null
 }

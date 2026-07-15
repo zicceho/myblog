6e903aea62674c388b1d9ca2e45c829890ad2e10
githubdudu <35301543+githubdudu@users.noreply.github.com>
2026-07-15 20:38:36 +1200

    fix: hexo theme aos refresh (#4274)

--- components/AOSAnimation.js
+++ components/AOSAnimation.js
@@ -1,21 +1,5 @@
 import { loadExternalResource } from '@/lib/utils'
-import { useRouter } from 'next/router'
 import { useEffect } from 'react'
-// import AOS from 'aos'
-
-const refreshAOS = () => {
-  if (!window.AOS) return
-
-  if (window.AOS.refreshHard) {
-    window.AOS.refreshHard()
-  } else if (window.AOS.refresh) {
-    window.AOS.refresh()
-  }
-}
-
-const refreshAOSAfterPaint = () => {
-  window.requestAnimationFrame(refreshAOS)
-}
 
 /**
  * 加载滚动动画
@@ -23,7 +7,6 @@ const refreshAOSAfterPaint = () => {
  * https://michalsnik.github.io/aos/
  */
 export default function AOSAnimation() {
-  const router = useRouter()
   const initAOS = () => {
     if (
       window.matchMedia &&
@@ -38,12 +21,10 @@ export default function AOSAnimation() {
     ]).then(() => {
       if (window.AOS) {
         window.AOS.init({
-          disableMutationObserver: true,
           debounceDelay: 100,
           throttleDelay: 120,
           once: true
         })
-        refreshAOSAfterPaint()
       }
     })
   }
@@ -55,17 +36,6 @@ export default function AOSAnimation() {
     const id = window.setTimeout(initAOS, 2000)
     return () => window.clearTimeout(id)
   }, [])
-
-  useEffect(() => {
-    const handleRouteChangeComplete = () => {
-      refreshAOSAfterPaint()
-    }
-
-    router.events.on('routeChangeComplete', handleRouteChangeComplete)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChangeComplete)
-    }
-  }, [router.events])
 
   return null
 }
